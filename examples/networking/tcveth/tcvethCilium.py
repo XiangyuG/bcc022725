3c720857da84f4a84aac63a669376d87c4c40a73
Tjaarda1 <abuning@pa.uc3m.es>
2026-01-13 09:53:46 -0800

    tcveth example: migrated code to custom directory to better differentiate bpf code from bcc loader

---
+++ examples/networking/tcveth/tcvethCilium.py
@@ -0,0 +1,84 @@
+from bcc import BPF
+from pyroute2 import IPRoute
+import pyroute2
+
+from kube_query import *
+
+
+
+def cleanup():
+    print("\n[*] Detaching TC and cleaning up...")
+
+    try:
+        # 删除 ingress filters（两个 parent）
+        ipr.tc("del-filter", "bpf", idx, ":1", parent="ffff:fff2")
+        ipr.tc("del-filter", "bpf", idx1, ":1", parent="ffff:fff2")
+        ipr.tc("del-filter", "bpf", idx2, ":1", parent="ffff:fff2")
+    except Exception:
+        pass
+
+    # try:
+    #     ipr.tc("del-filter", "bpf", idx, ":1", parent="ffff:fff3")
+    #     ipr.tc("del-filter", "bpf", idx1, ":1", parent="ffff:fff3")
+    #     ipr.tc("del-filter", "bpf", idx2, ":1", parent="ffff:fff3")
+    # except Exception:
+    #     pass
+
+    try:
+        # 删除 clsact qdisc
+        ipr.tc("del", "clsact", idx)
+        ipr.tc("del", "clsact", idx1)
+        ipr.tc("del", "clsact", idx2)
+    except Exception:
+        pass
+
+    if b:
+        b.cleanup()
+
+    print("[✓] Cleanup done.")
+    
+ipr = IPRoute()
+interface = "lxcb54b62d61434"
+interface1 = "lxc102115b89e79"
+interface2 = "lxc04e01eda2318"
+# Ensure the interface exists
+try:
+   idx = ipr.link_lookup(ifname=interface)[0]
+   idx1 = ipr.link_lookup(ifname=interface1)[0]
+   idx2 = ipr.link_lookup(ifname=interface2)[0]
+except IndexError:
+   print(f"Error: Interface {interface} not found. Is it created?")
+   exit(1)
+
+# Ensure clsact qdisc is added only once
+try:
+    ipr.tc("add", "clsact", idx)
+    ipr.tc("add", "clsact", idx1)
+    ipr.tc("add", "clsact", idx2)
+except Exception as e:
+    print(f"clsact qdisc already exists: {e}")
+
+# Attach to veth0 using TC
+try:
+    b = BPF(src_file = "tcveth.c", debug=0)
+    service_pod_mapping, services, pods = kube_query()
+    # TODO: Add automatically later
+    backend_set = b["backend_set"]
+    backend_set[backend_set.Key(0x0A000132)] = backend_set.Leaf(1)  # 10.0.1.110
+    backend_set[backend_set.Key(0x0A00012A)] = backend_set.Leaf(1)  # 10.0.1.42
+
+    print(service_pod_mapping)
+    fn = b.load_func("redirect_service", BPF.SCHED_CLS)
+    ipr.tc("add-filter", "bpf", idx, ":1", fd=fn.fd, name=fn.name, parent="ffff:fff2", classid=1)
+
+    fn = b.load_func("redirect_service", BPF.SCHED_CLS)
+    ipr.tc("add-filter", "bpf", idx1, ":1", fd=fn.fd, name=fn.name, parent="ffff:fff2", classid=1)
+
+    fn = b.load_func("redirect_service", BPF.SCHED_CLS)
+    ipr.tc("add-filter", "bpf", idx2, ":1", fd=fn.fd, name=fn.name, parent="ffff:fff2", classid=1)
+
+    print(f"BPF attached to {interface} - SCHED_CLS: OK")
+    print("Waiting for packets... Press Ctrl+C to stop.")
+    b.trace_print()
+finally:
+   cleanup()
